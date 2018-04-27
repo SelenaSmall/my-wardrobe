ed7107e314f670eb668a511872c092895249f5c0
SelenaSmall <selenawiththetattoo@gmail.com>
2018-04-27 18:31:59 +1000

    testing tags

--- spec/features/add_tag_spec.rb
+++ spec/features/add_tag_spec.rb
@@ -15,4 +15,23 @@ RSpec.describe 'The wearer creates a new tag', feature: true do
 
     expect(page.body).to have_content('day wear')
   end
+
+  # Ensure the same tag doesn't get created twice
+
+  # let!(:day_wear) { create(:tag, name: 'day wear') }
+
+  # scenario 'ensure day wear can only be added to tags one time' do
+  #   visit new_tag_path
+  #   expect(page.body).to have_content('New Tag')
+
+  #   within('form') do
+  #     fill_in 'tag', with: 'day wear'
+  #   end
+
+  #   click_button 'Create Tag'
+
+  #   visit tags_path
+
+  #   expect(page.body).to have_content('sunny')
+  # end
 end
